8f541d6e97e7921b16b48352749dc24027004b81
Anouer Hermassi <ahermass@stevens.edu>
2019-05-31 15:24:25 +0100

    Added find_path method

--- ch6_trees_graphs/graph_dictionary.py
+++ ch6_trees_graphs/graph_dictionary.py
@@ -33,6 +33,26 @@ def generate_edges(graph):
     return edges
 
 
+def find_path(graph, start, end, path=None):
+    if path is None:
+        path = []
+
+    path.append(start)
+
+    if start == end:
+        return path
+
+    if end in graph[start]:  # end node is immediate neighbor of start node
+        path.append(end)
+        return path
+
+    for adj in graph[start]:
+        if adj not in path: # if node hasn't been visited before
+            return find_path(graph, adj, end, path)  # new path contains the already visited nodes
+
+    return None
+
+
 if __name__ == '__main__':
     graph = defaultdict(list)
 
@@ -49,3 +69,6 @@ if __name__ == '__main__':
 
     print(generate_edges(graph))
 
+    print("Path from b to a: ", find_path(graph, 'b', 'a'))  # b -> c -> a
+
+    print("Path from a to f: ", find_path(graph, 'a', 'f'))
